8284117e76c0fcff4091de3ef623014e4594a593
Mihai <45673304+mihaicm93@users.noreply.github.com>
2026-02-17 21:28:29 +0100

    feat: minimise-to-tray-on-startup (#126)
    * feat: minimise-to-tray-on-startup
    
    Signed-off-by: Mihai <cristian@mihaimuresan.com>
    
    * feat: minimise-to-tray-on-startup
    
    Signed-off-by: Mihai <cristian@mihaimuresan.com>
    
    * fix: redundant call
    
    Signed-off-by: Mihai <cristian@mihaimuresan.com>
    
    * fix: redundant call
    
    Signed-off-by: Mihai <cristian@mihaimuresan.com>
    
    ---------
    
    Signed-off-by: Mihai <cristian@mihaimuresan.com>
    Co-authored-by: Mihai <cristian@mihaimuresan.com>

--- src/native/window.ts
+++ src/native/window.ts
@@ -36,6 +36,10 @@ const windowIcon = nativeImage.createFromDataURL(windowIconAsset);
  * Create the main application window
  */
 export function createMainWindow() {
+  // (CLI arg --hidden or config)
+  const startHidden =
+    app.commandLine.hasSwitch("hidden") || config.startMinimisedToTray;
+
   // create the window
   mainWindow = new BrowserWindow({
     minWidth: 300,
@@ -45,6 +49,7 @@ export function createMainWindow() {
     backgroundColor: "#191919",
     frame: !config.customFrame,
     icon: windowIcon,
+    show: !startHidden,
     webPreferences: {
       // relative to `.vite/build`
       preload: join(__dirname, "preload.js"),
